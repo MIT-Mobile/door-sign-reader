c7025865b55f7a917713092375659ff6d55e09b8
Brian Patt <blpatt@mit.edu>
2012-02-17 17:48:36 -0500

    draw borders around all the connectted objects

--- cv_extras.py
+++ cv_extras.py
@@ -1,12 +1,20 @@
 import cv
 
 class Label:
-    def __init__(self, id):
+    def __init__(self, id, color, x, y):
         self.id = id
         self.lowerLinkLabel = None
+        self.color = color
+        self.minX = x
+        self.maxX = x
+        self.minY = y
+        self.maxY = y
+
+    def isRoot(self):
+        return (self.lowerLinkLabel == None)
 
     def getMinimumLabel(self):
-        if self.lowerLinkLabel == None:
+        if self.isRoot():
             return self
         else:
             label = self.lowerLinkLabel.getMinimumLabel()
@@ -16,11 +24,30 @@ class Label:
     def getMinimumID(self):
         return self.getMinimumLabel().id
 
+    def setLowerLinkLabel(self, other):
+        other.addPoint(self.minX, self.minY)
+        other.addPoint(self.maxX, self.maxY)
+        self.lowerLinkLabel = other
+ 
     def linkLabel(self, other):
         if self.getMinimumID() > other.getMinimumID():
-           self.getMinimumLabel().lowerLinkLabel = other
+           self.getMinimumLabel().setLowerLinkLabel(other)
         elif self.getMinimumID() < other.getMinimumID():
-           other.getMinimumLabel().lowerLinkLabel = self
+           other.getMinimumLabel().setLowerLinkLabel(self)
+
+    def addPoint(self, x, y):
+        if self.isRoot():
+            if x < self.minX:
+                self.minX = x
+            if x > self.maxX:
+                self.maxX = x
+            if y < self.minY:
+                self.minY = y
+            if y > self.maxY:
+                self.maxY = y
+        else:
+            self.lowerLinkLabel.addPoint(x, y)
+
 
 def getMergedLabel(a, b):
     if a == None:
@@ -32,6 +59,7 @@ def getMergedLabel(a, b):
 def partiallyLabel(src):
     nextLabel = 0
     labelsGrid = [[0 for col in range(src.cols)] for row in range(src.rows)]    
+    allLabels = []
 
     for y in range(src.rows):
         for x in range(src.cols):
@@ -57,12 +85,14 @@ def partiallyLabel(src):
 
             if currentLabel:
                labelsGrid[y][x] = currentLabel
+               currentLabel.addPoint(x, y)
             else:
-               newLabel = Label(nextLabel)
+               newLabel = Label(nextLabel, currentColor, x, y)
                labelsGrid[y][x] = newLabel
+               allLabels.append(newLabel)
                nextLabel += 1
 
-    return labelsGrid
+    return labelsGrid, allLabels
 
 def simpleHash256(value):
     return 991 * (349 + value) % 256
@@ -72,11 +102,17 @@ def completeLabels(labelsGrid, dest):
         for x in range(dest.cols):
             dest[y, x] = simpleHash256(labelsGrid[y][x].getMinimumID())
 
-def labelImage(src, dest):
-    labelsGrid = partiallyLabel(src)
-    completeLabels(labelsGrid, dest)
+def drawRectangles(labels, dest):
+    for label in labels:
+        if label.isRoot():
+            cv.Rectangle(dest, (label.minX, label.minY), (label.maxX, label.maxY), simpleHash256(2*label.id), 2, cv.CV_AA)
             
-def max(image):
+def labelImage(src, dest):
+    labelsGrid, allLabels = partiallyLabel(src)
+    completeLabels(labelsGrid, dest)
+    drawRectangles(allLabels, dest)
+            
+def imageMax(image):
     max = -1
     for y in range(image.rows):
         for x in range(image.cols):
